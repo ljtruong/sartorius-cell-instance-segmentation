373b8beeecdfe03fc5ad8c091bc74a480c95c372
ljtruong <leon.truong1@gmail.com>
2021-10-30 22:53:07 +1100

    update training setup

--- cell_segmentation/models/mask_rcnn/sartorius_trainer.py
+++ cell_segmentation/models/mask_rcnn/sartorius_trainer.py
@@ -1,15 +1,28 @@
-from typing import DefaultDict
 from detectron2.engine import DefaultTrainer
-from detectron2.config import get_cfg
-from detectron2 import model_zoo
+from detectron2.data import (
+    DatasetMapper,
+    build_detection_test_loader,
+    build_detection_train_loader,
+)
+
+
+class TransformsMapper:
+    pass
 
 
 class SartoriusTrainer(DefaultTrainer):
-    def load_config(self, config_path: str) -> None:
-        """
-        Load a config file and merge it with the default config.
-        """
-        cfg = get_cfg()
-        cfg.merge_from_file(config_path)
-        cfg.freeze()
-        self.cfg = cfg
+    pass
+    # TODO add custom transforms
+    # @classmethod
+    # def build_train_loader(self, cfg):
+    #     """ """
+    #     mapper = TransformsMapper(cfg, is_train=True)
+    #     return build_detection_train_loader(cfg, mapper=mapper)
+
+    # @classmethod
+    # def build_test_loader(cls, cfg, dataset_name):
+    #     """
+    #     augment the test split too
+    #     """
+    #     mapper = TransformsMapper(cfg, is_train=False)
+    #     return build_detection_test_loader(cfg, dataset_name, mapper=mapper)
--- cell_segmentation/models/mask_rcnn/train.py
+++ cell_segmentation/models/mask_rcnn/train.py
@@ -1,39 +1,57 @@
-from cell_segmentation.models.mask_rcnn.sartorius_trainer import SartoriusTrainer
-from detectron2.config import get_cfg
-from cell_segmentation.data.loader import Loader
-from cell_segmentation.data.converter.satorus2coco import Satorus2COCO
 import os
 
+from detectron2.config import get_cfg
+import click
 
-def main():
-
-    cfg = get_cfg()
-
-    # trainer.load_config(
-    #     config_path="cell_segmentation/models/mask_rcnn/configs/sartorius/sartorius_mask_rcnn_R_50_FPN_3x.yaml"
-    # )
-    config_path = os.path.join(
-        os.getcwd(),
-        "cell_segmentation/models/mask_rcnn/configs/mask_rcnn_R_50_FPN_3x.yaml",
-    )
-    cfg.merge_from_file(config_path)
-
-    trainer = SartoriusTrainer(cfg)
-    trainer.resume_or_load(resume=False)
-    trainer.train()
+from cell_segmentation.data.converter.satorus2coco import Satorus2COCO
+from cell_segmentation.data.datasets import Datasets
+from cell_segmentation.data.loader import Loader
+from cell_segmentation.models.mask_rcnn.sartorius_trainer import SartoriusTrainer
 
 
-if __name__ == "__main__":
-
+def load_dataset():
     data_loader = Loader()
     satorus_converter = Satorus2COCO()
     df = data_loader.load_static_dataset(filepath="data/train.csv")
     df = data_loader.preprocess_static_dataset(df)
 
-    # TODO split train and val
-    df = df[:1000]
-    train_dataset = data_loader.build_microscopyimage_from_dataframe(df)
-    satorus_converter.register_instances("train", train_dataset)
+    df = df[:1000]  # Temporary
 
-    main()
-    print("here")
+    train_df, val_df = Datasets.generate_examples(df, train_split=0.8, test_split=0.2)
+    train_dataset = data_loader.build_microscopyimage_from_dataframe(train_df)
+    val_df = data_loader.build_microscopyimage_from_dataframe(val_df)
+
+    satorus_converter.register_instances("train", train_dataset)
+    satorus_converter.register_instances("val", train_dataset)
+
+
+def main(config: str):
+
+    cfg = get_cfg()
+    config_path = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "..",
+        "..",
+        config,
+    )
+    cfg.merge_from_file(config_path)
+
+    load_dataset()
+    trainer = SartoriusTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+
+@click.command()
+@click.option(
+    "--config",
+    type=str,
+    default="cell_segmentation/models/mask_rcnn/configs/mask_rcnn_R_50_FPN_3x.yaml",
+)
+def train(config: str):
+    main(config)
+
+
+if __name__ == "__main__":
+    train()
