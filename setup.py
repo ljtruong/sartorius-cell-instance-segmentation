05a99774210470807583659f11d9dd54a39c7453
ljtruong <leon.truong1@gmail.com>
2021-10-30 21:59:17 +1100

    update environment setup packages

--- setup.py
+++ setup.py
@@ -7,4 +7,12 @@ setup(
     description="sartorius cell instance segmentation kaggle competition",
     author="Leon Truong",
     license="MIT",
+    install_requires=[
+        "black",
+        "numpy==1.21.3",
+        "pandas==1.3.4",
+        "torch@https://download.pytorch.org/whl/cu113/torch-1.10.0%2Bcu113-cp39-cp39-linux_x86_64.whl",
+        "torchvision@https://download.pytorch.org/whl/cu113/torchvision-0.11.1%2Bcu113-cp39-cp39-linux_x86_64.whl",
+        "detectron2@https://dl.fbaipublicfiles.com/detectron2/wheels/cu113/torch1.10/index.html",
+    ],
 )
