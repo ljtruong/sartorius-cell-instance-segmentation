66b605cbd70a0ee9aa6e0ea89ce24440a027c3e1
ljtruong <leon.truong1@gmail.com>
2021-10-30 21:22:16 +1100

    add unittesting

---
+++ tests/data/test_dataset.py
@@ -0,0 +1,20 @@
+import unittest
+
+import pandas as pd
+import numpy as np
+
+from cell_segmentation.data.datasets import Datasets
+
+
+class TestDataset(unittest.TestCase):
+    def test_generate_examples(self):
+
+        df = pd.DataFrame(np.random.random(100))
+        train_df, test_df = Datasets.generate_examples(df)
+
+        self.assertEqual(len(train_df), 80)
+        self.assertEqual(len(test_df), 20)
+
+
+if __name__ == "__main__":
+    unittest.main()
