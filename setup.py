eeb74913c0b1728b56620d8022c2fb626c53ea2f
ljtruong <leon.truong1@gmail.com>
2021-11-01 22:36:47 +1100

    revert to py 3.7 for kaggle kernel

--- setup.py
+++ setup.py
@@ -12,8 +12,9 @@ setup(
         "click==8.0.3",
         "numpy==1.21.3",
         "pandas==1.3.4",
-        "torch@https://download.pytorch.org/whl/cu113/torch-1.10.0%2Bcu113-cp39-cp39-linux_x86_64.whl",
-        "torchvision@https://download.pytorch.org/whl/cu113/torchvision-0.11.1%2Bcu113-cp39-cp39-linux_x86_64.whl",
-        # "detectron2@https://dl.fbaipublicfiles.com/detectron2/wheels/cu113/torch1.10/index.html",
+        "torch@https://download.pytorch.org/whl/cu113/torch-1.10.0%2Bcu113-cp37-cp37m-linux_x86_64.whl",
+        "torchvision@https://download.pytorch.org/whl/cu113/torchvision-0.11.1%2Bcu113-cp37-cp37m-linux_x86_64.whl",
+        "detectron2@https://dl.fbaipublicfiles.com/detectron2/wheels/cu113/torch1.10/detectron2-0.6%2Bcu113-cp37-cp37m-linux_x86_64.whl",
     ],
+    python_requires=">=3.7",
 )
